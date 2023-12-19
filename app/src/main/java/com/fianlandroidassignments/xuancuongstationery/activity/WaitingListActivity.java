da5dd30b19cdc92b9eec3c2b4846599568a3e43a
loftysky <lieuht2040@gmail.com>
2023-12-19 16:01:33 +0700

    Da them button

--- app/src/main/java/com/fianlandroidassignments/xuancuongstationery/activity/WaitingListActivity.java
+++ app/src/main/java/com/fianlandroidassignments/xuancuongstationery/activity/WaitingListActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.fianlandroidassignments.xuancuongstationery.R;
 import com.fianlandroidassignments.xuancuongstationery.adapter.ArrayCategoryAdapter;
@@ -20,6 +21,8 @@ public class WaitingListActivity extends AppCompatActivity {
     ArrayAdapter<String> adapterExsItems;
     ListView listViewImport;
     ListView listViewSell;
+    RelativeLayout importForm;
+    RelativeLayout sellForm;
     ImportAdapter importAdapter;
 
     @Override
@@ -34,11 +37,11 @@ public class WaitingListActivity extends AppCompatActivity {
         //handle hide/show import or sell view based on status
         autoCompleteTextViewWaitingListStatus.setOnItemClickListener((parent, view, position, id) -> {
             if ("Import List".equals(adapterExsItems.getItem(position))) {
-                listViewImport.setVisibility(View.VISIBLE);
-                listViewSell.setVisibility(View.INVISIBLE);
+                importForm.setVisibility(View.VISIBLE);
+                sellForm.setVisibility(View.INVISIBLE);
             } else if ("Sell List".equals(adapterExsItems.getItem(position))) {
-                listViewSell.setVisibility(View.VISIBLE);
-                listViewImport.setVisibility(View.INVISIBLE);
+                sellForm.setVisibility(View.VISIBLE);
+                importForm.setVisibility(View.INVISIBLE);
             }
         });
 
@@ -65,5 +68,7 @@ public class WaitingListActivity extends AppCompatActivity {
         autoCompleteTextViewWaitingListStatus = findViewById(R.id.autoCompleteImportExists);
         listViewImport = findViewById(R.id.listViewImport);
         listViewSell = findViewById(R.id.listViewSell);
+        importForm = findViewById(R.id.importForm);
+        sellForm = findViewById(R.id.sellForm);
     }
 }
